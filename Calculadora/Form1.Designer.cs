e6c06d612f6b357aa343053b79a76dc928cc114f
Kevin Diego <kevindiegodasilvasousa@gmail.com>
2022-09-20 09:07:43 -0300

    funcionalidades de calculo
    adicionado funcionalidades no formulário para calculo e interações de melhoria após realizar operação

--- Calculadora/Form1.Designer.cs
+++ Calculadora/Form1.Designer.cs
@@ -28,13 +28,74 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.cmd_calcular = new System.Windows.Forms.Button();
+            this.cmd_sair = new System.Windows.Forms.Button();
+            this.txt_operacao = new System.Windows.Forms.TextBox();
+            this.lbl_algoritmo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cmd_calcular
+            // 
+            this.cmd_calcular.Location = new System.Drawing.Point(12, 73);
+            this.cmd_calcular.Name = "cmd_calcular";
+            this.cmd_calcular.Size = new System.Drawing.Size(100, 40);
+            this.cmd_calcular.TabIndex = 1;
+            this.cmd_calcular.Text = "Calcular";
+            this.cmd_calcular.UseVisualStyleBackColor = true;
+            this.cmd_calcular.Click += new System.EventHandler(this.cmd_calcular_Click);
+            // 
+            // cmd_sair
+            // 
+            this.cmd_sair.Location = new System.Drawing.Point(133, 73);
+            this.cmd_sair.Name = "cmd_sair";
+            this.cmd_sair.Size = new System.Drawing.Size(93, 40);
+            this.cmd_sair.TabIndex = 2;
+            this.cmd_sair.Text = "Sair";
+            this.cmd_sair.UseVisualStyleBackColor = true;
+            this.cmd_sair.Click += new System.EventHandler(this.cmd_sair_Click);
+            // 
+            // txt_operacao
+            // 
+            this.txt_operacao.Location = new System.Drawing.Point(12, 34);
+            this.txt_operacao.MaxLength = 30;
+            this.txt_operacao.Name = "txt_operacao";
+            this.txt_operacao.Size = new System.Drawing.Size(214, 20);
+            this.txt_operacao.TabIndex = 0;
+            this.txt_operacao.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txt_operacao_KeyDown);
+            // 
+            // lbl_algoritmo
+            // 
+            this.lbl_algoritmo.AutoSize = true;
+            this.lbl_algoritmo.Location = new System.Drawing.Point(12, 9);
+            this.lbl_algoritmo.Name = "lbl_algoritmo";
+            this.lbl_algoritmo.Size = new System.Drawing.Size(100, 13);
+            this.lbl_algoritmo.TabIndex = 3;
+            this.lbl_algoritmo.Text = "Escreva o algoritmo";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(242, 125);
+            this.Controls.Add(this.lbl_algoritmo);
+            this.Controls.Add(this.txt_operacao);
+            this.Controls.Add(this.cmd_sair);
+            this.Controls.Add(this.cmd_calcular);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CALCULADORA1.0";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button cmd_calcular;
+        private System.Windows.Forms.Button cmd_sair;
+        private System.Windows.Forms.TextBox txt_operacao;
+        private System.Windows.Forms.Label lbl_algoritmo;
     }
 }
 
--- Calculadora/Form1.cs
+++ Calculadora/Form1.cs
@@ -16,5 +16,81 @@ namespace Calculadora
         {
             InitializeComponent();
         }
+
+        private void cmd_calcular_Click(object sender, EventArgs e)
+        {
+            //executa o calculo
+            int parcela1;
+            int parcela2;
+            string operacao;
+            int resultado = -11111111;
+
+            string algoritmo = txt_operacao.Text;
+            string sinais = "+-*/";
+            int idenx = 0;
+
+            foreach (var c in algoritmo)
+            {
+                foreach (var s in sinais)
+                {
+                    if (c == s)
+                    {
+                        //define os 3 elementos da operaçao
+                        parcela1 = int.Parse(algoritmo.Substring(0, idenx));
+                        //sinal da operacao
+                        operacao = c.ToString();
+
+                        parcela2 = int.Parse(algoritmo.Substring(idenx + 1));
+
+                        //analise de operacao
+                        switch (operacao)
+                        {
+                            case "+":
+                                //adicao
+                                resultado = parcela1 + parcela2;
+                                break;
+                            case "-":
+                                //subtração
+                                resultado = parcela1 - parcela2;
+                                break;
+                            case "*":
+                                //multiplicação
+                                resultado = parcela1 * parcela2;
+                                break;
+                            case "/":
+                                //divisao
+                                resultado = parcela1 / parcela2;
+                                break;
+                        }
+                        break;
+                    }
+                }
+                if (resultado != -11111111)
+                    break;
+                idenx++;
+            }
+            //mostra resultado
+            MessageBox.Show(algoritmo + " = " + resultado);
+
+            //limpa a textbox
+            txt_operacao.Text = "";
+            txt_operacao.Focus();
+        }
+
+        private void cmd_sair_Click(object sender, EventArgs e)
+        {
+            //encerra a aplicaçao
+            Application.Exit();
+        }
+
+        private void txt_operacao_KeyDown(object sender, KeyEventArgs e)
+        {
+            //ao clicar em retornar realiza operacao, exeto se nao existir texto
+
+            if (txt_operacao.Text == "") return;
+
+            if (e.KeyCode == Keys.Return)
+                cmd_calcular_Click(cmd_calcular, EventArgs.Empty);
+        }
     }
 }
